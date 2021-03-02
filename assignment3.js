db0ffa4bfe5e27ac4d57a9928502dfe6d6bee738
Duncan-Kyhl <65314384+Duncan-Kyhl@users.noreply.github.com>
2021-03-02 06:47:15 -0600

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,39 @@
+// cityWhile
+// let cities = []
+
+// do {
+//     cities.push(prompt('Enter a city:'))
+// } while (city != 'stop' || city != 'Stop' || city != 'STOP')
+
+// console.log(cities)
+
+
+
+// forReverseStates
+
+// // create array with states provided
+// const states = ['Nebraska', 'Iowa', 'Minnesota', 'South Dakota', 'North Dakota', 'Kansas', 'Oklahoma', 'Texas']
+
+// // step through the array backwards, printing each element
+// for (let i = states.length - 1; i >= 0; i--) {
+//     console.log(states[i])
+// }
+
+// functionCompare
+
+// let password = prompt('Enter your new password:')
+
+// function compare(pass1, pass2) {
+//     let isEqual = pass1.localeCompare(pass2)
+//     return (isEqual == 0) ? 'The passwords are the same.' : 'The passwords are not the same.'
+// }
+
+// console.log(compare('password', password))
+
+
+
+// TBA from Week 6 material
+
+another problem will be posted
+
+// Extra Credi: Nested Loops
